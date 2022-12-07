1f9f117590b47c92b0b3d419f80c1e8ab2615760
Bruno Coelho <eu.brunocoelho94@gmail.com>
2022-12-07 12:51:25 -0300

    adding features

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -12,3 +12,16 @@ var swiper = new Swiper('.mySwiper', {
         }
     }
 });
+
+var mobileBtn = document.querySelector('#mobileBtn');
+
+mobileBtn.addEventListener('click', () => {
+    document.querySelector('.sidenav__background').style.display = 'block';
+    setTimeout(() => {
+        document.querySelector('.sidenav').style.width = '290px';
+    }, 200);
+})
+
+/**
+ * NOT COMPLETED
+ */
