607bd2203daeee21cf5a3e0e73ae8cc682d2aba8
Bruno Coelho <eu.brunocoelho94@gmail.com>
2022-12-07 14:44:54 -0300

    side navigation finished

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,27 +1,37 @@
-var swiper = new Swiper('.mySwiper', {
-    slidesPerView: 'auto',
-    loop: true,
-    loopFillGroupWithBlank: true,
-    spaceBetween: 20,
-    breakpoints: {
-        320: {
-            spaceBetween: 10
-        },
-        480: {
-            spaceBetween: 20
-        }
-    }
-});
-
-var mobileBtn = document.querySelector('#mobileBtn');
-
-mobileBtn.addEventListener('click', () => {
-    document.querySelector('.sidenav__background').style.display = 'block';
-    setTimeout(() => {
-        document.querySelector('.sidenav').style.width = '290px';
-    }, 200);
-})
+var swiper = new Swiper('.mySwiper', { slidesPerView: 'auto', loop: true, loopFillGroupWithBlank: true, spaceBetween: 20, breakpoints: { 320: { spaceBetween: 10 }, 480: { spaceBetween: 20 } } });
 
 /**
- * NOT COMPLETED
+ * Side navigation
  */
+
+const windowOverlay = document.querySelector('.window-overlay');
+const sidenav = document.querySelector('.sidenav');
+const sidenavOpenBtn = document.querySelector('.header__mobile-button');
+const sidenavCloseBtn = document.querySelector('.sidenav__close-button');
+
+sidenavOpenBtn.addEventListener('click', () => {
+    openSidenav();
+
+    sidenavCloseBtn.addEventListener('click', () => closeSidenav());
+    window.addEventListener('click', (event) => {
+        if (event.target == windowOverlay) {
+            closeSidenav();
+        }
+    })
+});
+
+var openSidenav = () => {
+    windowOverlay.style.display = 'block';
+
+    setTimeout(() => {
+        sidenav.style.left = '0';
+    }, 200);
+};
+
+var closeSidenav = () => {
+    sidenav.style.left = '-290px';
+
+    setTimeout(() => {
+        windowOverlay.style.display = 'none';
+    }, 200);
+}
