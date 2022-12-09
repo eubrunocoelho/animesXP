bb2e1eba7df2fdcbc347251a732a064fe9c9bbf2
Bruno Coelho <eu.brunocoelho94@gmail.com>
2022-12-09 13:53:08 -0300

    starting anime-list.html

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,3 +1,9 @@
+/**
+ * =================================================================================
+ * NOT COMPLETED - BASE
+ * =================================================================================
+ */
+
 var swiper = new Swiper('.mySwiper', { slidesPerView: 'auto', loop: true, loopFillGroupWithBlank: true, spaceBetween: 20, breakpoints: { 320: { spaceBetween: 10 }, 480: { spaceBetween: 20 } } });
 
 /**
