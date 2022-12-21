71d4a97b150624a1d0aa506ed7dde76536c5d52a
Bruno Coelho <eu.brunocoelho94@gmail.com>
2022-12-21 16:42:02 -0300

    some adjustments

---
+++ assets/javascript/swiper.js
@@ -0,0 +1,1 @@
+var swiper = new Swiper('.mySwiper', { slidesPerView: 'auto', loop: true, loopFillGroupWithBlank: true, spaceBetween: 20, breakpoints: { 320: { spaceBetween: 10 }, 480: { spaceBetween: 20 } } });
